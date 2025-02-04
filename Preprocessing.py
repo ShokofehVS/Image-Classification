359314b4441b62d8c2fb704e3568be18cbcc388e
ShokofehVS <sh.vahidian@gmail.com>
2025-02-04 09:44:55 +0100

    Commenting and adding more text for README.md

--- Preprocessing.py
+++ Preprocessing.py
@@ -8,15 +8,12 @@ from PIL import Image
 def main(data_dir, preprocessed_data_dir):
     # Preprocessing function that
     # 1. Makes directory to store preprocessed images
-    # 2: Categorizes the dataset into two cats and dogs
-    # 2. Ensures input images are 64x64 pixels
+    # 2: Categorizes the dataset into two cats and dogs (ignoring horses)
+    # 3. Ensures input images are 64x64 pixels
     # 3. Converts images to grayscale (1 channel)
-    # 4. Additional preprocessing steps
-    # 5: Saves final results into the defined directory (refer to no. 1)
+    # 4: Saves final results into the defined directory (refer to no. 1)
 
     # (1)
-    if os.path.exists(preprocessed_data_dir):
-        shutil.rmtree(preprocessed_data_dir)
     os.makedirs(preprocessed_data_dir, exist_ok=True)
 
     # (2)
@@ -24,14 +21,20 @@ def main(data_dir, preprocessed_data_dir):
     for category in categories:
         os.makedirs(os.path.join(preprocessed_data_dir, category), exist_ok=True)
 
-    #  (3, 4)
+    #  (3)
     transform = transforms.Compose([
         transforms.Resize((64, 64)),
         transforms.Grayscale(num_output_channels=1)
     ])
 
-    #  (5)
+    #  (4)
     for category in categories:
+        # in our Preprocessed dataset (dest_path) we have exact category like in the train dataset (src_path):
+        # os.path.join: concatenate multiple paths such as data_dir, preprocessed_data_dir with suitable category
+        # os.listdir:   list all files and directories in a specific directory, here src_path to go through all images
+        # Image.open:   open an image files derived from Pillow library, here img_path for specific category
+        # transform:    transformation of the source image into 64x64 pixels, and grayscale (1 channel)
+        # save:         save the preprocessed image into its path, here dest_path
         src_path  = os.path.join(data_dir, category)
         dest_path = os.path.join(preprocessed_data_dir, category)
 
@@ -49,7 +52,7 @@ def main(data_dir, preprocessed_data_dir):
 
 if __name__ == '__main__':
     # Path to the file where datasets after extraction and preprocessing respectively exist
-    data_dir = "./Dataset/train"
+    data_dir              = "./Dataset/train"
     preprocessed_data_dir = "./Preprocessed_animal_data/train"
 
     # Run the main function to preprocess the training dataset
--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # Image-Classification
+In this project, we implement an **image classification model** to distinguish between two categories of images (**cats
+vs. dogs**) in **PyTorch**. Then, we **train** the model, **evaluate** and **interpret** the results.
+
+We develop based on the [ResNet model](https://github.com/kuangliu/pytorch-cifar/blob/master/models/resnet.py); while configuring it to match out preprocessed input and the number of classes. After training model on train dataset, we log and plot the results for each epoch. Finally, we test the model on a single image in the test dataset to see the predicted class as well as the confidence score.
+
+## Installation
+First you need to ensure that all packages have been installed.
++ See `requirements.txt`
++ torch>=2.4.1
++ torchvision>=0.19.1
++ matplotlib>=3.7.1
++ scikit-learn>=1.1.1
++ Pillow>=9.2.0
+
+You can clone this repository:
+
+	   > git clone https://github.com/ShokofehVS/Image-Classification.git
+
+If you miss something you can simply type:
+
+	   > pip install -r requirements.txt
+
+If you have all dependencies installed:
+
+	   > pip3 install .
+
+## Dataset
+[Animal Classification Dataset (Cats, Dogs, Horses)](https://www.kaggle.com/datasets/arifmia/animal/data) involves three common animal categories: Cat, Dog, and Horse.  It includes a total of 1,763 images split into three subsets: Train, Validation (Val), and Test. 
+
+# Image Classification Steps:
+## Step 1: Preprocessing
+After accommodating an extracted dataset into a **Dataset** folder, we remove **horse** category; this can be done by writing a code in which define exclusively two categories of **dogs** and **cats**:
+
+Further, we transform the training dataset composing of images of cats and dogs into 64x64 pixels and grayscale (1 channel):
+
+`transform = transforms.Compose([
+        transforms.Grayscale(num_output_channels=1),
+        transforms.Resize((64, 64)),
+        transforms.ToTensor()
+    ])`
+
+The script for running preprocessing is `Preprocessing.py`. However, we devoted a space called `Preprocessed_animal_data` for this offline preprocessing on training datasets.
+
+Despite an offline preprocessing, we additionally can preprocess online in ``Training.py`` before starting training. 
+
+## Step 2: Training
